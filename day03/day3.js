e6bd403d1d8f3b760696e2d3772b827660a162fc
Ileyassu <ilyas.benaissa@um5r.ac.ma>
2023-07-27 20:02:33 +0100

    Exercise 1 done

--- day03/day3.js
+++ day03/day3.js
@@ -90,4 +90,22 @@ console.log(now.getTime())
 
 //Exercises: Level 2
 
+// Write a script that prompt the user to enter base and height of the
+//triangle and calculate an area of a triangle 
+//(area = 0.5 x b x h).
 
+let base = prompt('Write the Base','20'), 
+height = prompt('Write Height','10'), area = 0.5 * base * height
+console.log(area)
+
+/**Write a script that prompt the user to enter side a, 
+ * side b, and side c of the triangle and and calculate 
+ * the perimeter of triangle (perimeter = a + b + c) */
+
+let a = parseInt(prompt('Enter a side'));
+let b = parseInt(prompt('Enter a side'));
+let c = parseInt(prompt('Enter a side'));
+
+let perimeteur = a + b + c;
+
+console.log(perimeteur.typeof)
