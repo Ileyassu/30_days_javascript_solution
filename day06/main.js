df207787081deda2e96e5f8b20e9dbd17cf6693d
Ileyassu <ilyas.benaissa@um5r.ac.ma>
2023-08-03 17:38:46 +0100

    New commit for day05

---
+++ day06/main.js
@@ -0,0 +1,67 @@
+//Exercises: Level 1
+
+const countries = require('./data/countries')
+const webTechs = require('./data/webTechs')
+
+//1
+
+for(let i = 0; i<10; i++){
+    console.log(i)
+}
+let y = 0;
+do {
+    console.log(y)
+    y++;
+} while (y<10);
+
+let i = 0;
+
+while(i<10){
+    console.log(i)
+    i++;
+}
+
+//2
+for(let i = 10; i>0; --i){
+    console.log(i)
+}
+
+
+do{
+    console.log(i)
+    --i;
+}while(i>0)
+y = 10
+while(y<0){
+    console.log(y)
+    --y;
+}
+
+//3
+
+for(let n = 0; n < 5; n++){
+    n++
+}
+
+//4
+let hashString = '#';
+let emtyString = '';
+let x = 0;
+while(x < 5){
+    emtyString = emtyString + hashString;
+    x++;
+    console.log(emtyString)
+}
+
+//5
+
+x = 0
+let multiplication
+while(x<10){
+    x++
+    multiplication = x*x
+    console.log(`${x} * ${x} = ${multiplication}`)
+}
+
+//6
+
